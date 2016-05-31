ed7bdd0cc1415419599240662d4f6426a14c065a
Jared M. Peterson <jared@peteyinc.com>
2016-05-31 12:27:36 -0600

    css

--- js/timeDirective.js
+++ js/timeDirective.js
@@ -2,9 +2,13 @@ angular.module("timeApp").directive("showTime", function() {
   return {
     restrict: 'E',
     template: "<div>The current time is {{time}}</div>",
+    scope: {},
     link: function(scope, element, attrs) {
         var currentTime = new Date();
         scope.time = currentTime.toString();
+        element.css({
+          color: attrs.color,
+        });
       }
   };
 });
